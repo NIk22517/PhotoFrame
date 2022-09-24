f82e387eae4bc713d041c8953ce86c935d07afe0
unknown <nik22517@gmail.com>
2022-09-24 15:38:27 +0530

    logo

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
-import Logo from "./Logo";
 
 const NavBar = ({ setFile }) => {
   const handleClick = () => {
@@ -11,7 +10,11 @@ const NavBar = ({ setFile }) => {
     <Nav>
       <div className='nav-logo'>
         <Link to='/' onClick={handleClick}>
-          <Logo />
+          <h5>
+            <span className='sdm'>SDM</span>{" "}
+            <span className='rural'>RURAL</span>{" "}
+            <span className='shimla'>SHIMLA</span>{" "}
+          </h5>
         </Link>
       </div>
       <div className='nav-link'>
@@ -43,8 +46,59 @@ const Nav = styled.nav`
   .nav-link {
     margin-right: 5rem;
   }
-  .nav-link a {
+  a h5 {
     color: blue;
+    position: relative;
+    z-index: 2;
+  }
+
+  h5 {
+    text-align: center;
+    position: relative;
+    cursor: pointer;
+    padding: 0.5rem;
+  }
+
+  h5::before {
+    content: "";
+    width: 100%;
+    height: 100%;
+    background-image: linear-gradient(
+      to bottom,
+      #ff9933,
+      #ff9c7c,
+      #ffadb9,
+      #ffc7e7,
+      #ffe2fe,
+      #ebe1ff,
+      #d5e0ff,
+      #bfdffc,
+      #68ceea,
+      #00bcbc,
+      #00a572,
+      #138808
+    );
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    inset: 0 0 0 0;
+    z-index: -1;
+    transition: transform 0.3s ease;
+    border-radius: 5px;
+  }
+
+  h5::before {
+    transform: scaleX(0);
+    transform-origin: bottom right;
+  }
+
+  @media (hover: hover) {
+    h5:hover::before {
+      transform: scaleX(1);
+      transform-origin: bottom left;
+    }
   }
 
   @media (max-width: 676px) {
